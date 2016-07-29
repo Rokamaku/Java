e942d0c25b59b69d3b6929d4657e2670cd35d71f
rokamaku <hoanganh1396@outlook.com>
2016-07-29 23:10:38 +0700

    More optimal

--- mathDice.java
+++ mathDice.java
@@ -9,10 +9,9 @@ class mathDice
 {
 	private static Scanner scanIn;
 	// opsArr store the set of operators which permunated
-	public static ArrayList<String> opsArr = new ArrayList<String>();
+	public static Set<String> opsArr = new HashSet<String>();
 	private static double closestVal;	
 	private static double valTar;	
-	//initlize delta = positive_infinitY
 	private static double delta = Double.POSITIVE_INFINITY;
 	private static String closestExprs = new String();
 	public static void main (String[] args)
@@ -31,6 +30,7 @@ class mathDice
 		{
 			System.out.println("Error!");
 		}
+		// create a list of number in type of array of string
 		StringTokenizer str = new StringTokenizer(setNum," ");
 		ArrayList<String> operands = new ArrayList<String>();
 		while (str.hasMoreTokens())
@@ -40,6 +40,7 @@ class mathDice
 		System.out.println("Closest expression");
 		System.out.println(closestExprs + " = " + closestVal);
 	}
+	//create an array of operator first and store then create permutated operandsand merge with array of operators
 	private static void solveMathDice(ArrayList<String> operands, char[] operators)
 	{
 		// print 1 length operand case
@@ -86,6 +87,7 @@ class mathDice
 	{
 		 if (begin == end)
 		    {
+		    	//convert the permutated operators into string and store in array of operators string
 			 	String ops = new String(operators);
 		    	opsArr.add(ops);
 		    }
@@ -93,9 +95,10 @@ class mathDice
 		    {
 		        for (int i = begin; i <= end; i++)
 		        {
+		        	//swap from i >= begin
 		            swap(operators, begin, i);
-		            PermutationOps(operators, begin + 1, end);
-		            swap(operators, begin, i);
+		            PermutationOps(operators, begin + 1, end); 
+		            swap(operators, begin, i); //backtracking
 		        }
 		    }
 	}
@@ -121,9 +124,10 @@ class mathDice
 	{
 	    if (begin == end)
 	    {
-	        for (int i = 0; i < opsArr.size(); i++)
+	    	Iterator<String> iterator = opsArr.iterator();
+	        while (iterator.hasNext())
 	        {
-	        	char[] operators = opsArr.get(i).toCharArray();
+	        	char[] operators = ((String) iterator.next()).toCharArray();
 	        	createExprs(perm, operators, "", perm.size() - 1, perm.size(), 0, 0);
 	        }
 	    }
@@ -137,8 +141,16 @@ class mathDice
 	        }
 	    }
 	}
+	// merge each permutated operand with each operators in array of operators stored to create a legal expression
+	//String exprs: legal expression
+	//int ops: number of operator = length of operand - 1
+	//int nums: number of operands
+	//int curOps: current operator position
+	//int curNum: current operand position
 	private static void createExprs(ArrayList<String> operands, char[] operators, String exprs, int ops, int nums, int curOps, int curNums)
 	{
+		//when nums = 0 add there is no operand to add => start to add the remain operators
+		//if ops = 1 then add operator to expression , make sure that the last of string is always an operator 
 		if (nums == 0)
 	        if (ops == 1)
 	        {
@@ -152,15 +164,22 @@ class mathDice
 	        
 	        {
 	          createExprs(operands, operators, exprs + operands.get(curNums) + " ", ops, nums - 1, curOps, curNums + 1);
+	          //when there is only one operand and operator go back to previous
 	          if (ops > nums)
 	            createExprs(operands, operators, exprs + operators[curOps] + " ", ops - 1, nums, curOps + 1, curNums);
 	        }
 	}
+	// evaluate the value of expression created
 	private static void evaExprs(String exprs)
 	{
 		Stack<Double> St = new Stack<Double>();
 		char[] Exprsch = exprs.toCharArray();
-		int flag = 0;
+		//no calErr to know that whether the calculation success or not
+		boolean noCalErr = false;
+		//read the whole expression
+		//if it is a number push to stack 
+		//or pop two number from the stack to calculate
+		//and then push back to the stack
 		for (int i = 0; i < Exprsch.length; i++)
 		{
 			if (Exprsch[i] == ' ')
@@ -179,7 +198,7 @@ class mathDice
 				if (b == 0 && Exprsch[i] == '/' )
 				{
 					System.out.println(" Cannot divide by zero!");
-					flag++;
+					noCalErr = true;
 					break;
 				}
 				
@@ -187,7 +206,7 @@ class mathDice
 				St.push(result);
 			}
 		}
-		if (flag == 0)
+		if (noCalErr == false)
 		{
 			double lResult = St.pop();
 			System.out.println(" = " + lResult);
